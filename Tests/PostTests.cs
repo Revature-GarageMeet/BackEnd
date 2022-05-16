f8e6cc56729f898ce57c0c0317b23bedcbd3a764
reyjvergara <rey.j.vergara@gmail.com>
2022-05-16 09:41:25 -0700

    Create PostTest
    Co-Authored-By: blchilds55 <71292390+blchilds55@users.noreply.github.com>

---
+++ Tests/PostTests.cs
@@ -0,0 +1,15 @@
+using Xunit;
+using Moq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
+using Models;
+using Datalayer;
+using WebAPI.Controllers;
+
+namespace Tests;
+
+public class PostsTests
+{
+    
+}
