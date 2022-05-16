94c1a61b9024ab64eccf0857762043183642cfcb
NovaFlash321 <novaflash321@gmail.com>
2022-05-16 11:07:56 -0400

    fixed route issue

--- WebAPI/Controllers/BandMemberController.cs
+++ WebAPI/Controllers/BandMemberController.cs
@@ -1,38 +1,38 @@
-//using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 
-//namespace WebAPI.Controllers;
+namespace WebAPI.Controllers;
 
-//// BandMembers Controller
-//// Created By: Bailey
-//[Route("[controller]")]
-//[ApiController]
-//public class BandMemberController : ControllerBase
-//{
-//    private readonly DBInterface _dl;
+// BandMembers Controller
+// Created By: Bailey
+[Route("[controller]")]
+[ApiController]
+public class BandMemberController : ControllerBase
+{
+   private readonly DBInterface _dl;
 
-//    public BandMemberController(DBInterface dl)
-//    {
-//        _dl = dl;
-//    }
+   public BandMemberController(DBInterface dl)
+   {
+       _dl = dl;
+   }
 
-//    // Gets all band members for a Single band ~Bailey
-//    [HttpGet]
-//    public async Task<List<BandMember>> GetAll(int bandId)
-//    {
-//        return await _dl.GetAllBandMembers(bandId);
-//    }
+   // Gets all band members for a Single band ~Bailey
+   [HttpGet]
+   public async Task<List<BandMember>> GetAll(int bandId)
+   {
+       return await _dl.GetAllBandMembers(bandId);
+   }
 
-//    [HttpPost]
-//    public async Task<BandMember> Post(BandMember newMember)
-//    {
-//        newMember.dateJoined = DateTime.UtcNow;
-//        return await _dl.CreateBandMember(newMember);
-//    }
+   [HttpPost]
+   public async Task<BandMember> Post(BandMember newMember)
+   {
+       newMember.dateJoined = DateTime.UtcNow;
+       return await _dl.CreateBandMember(newMember);
+   }
 
-//    [HttpDelete]
-//    public async Task Delete(BandMember memberToDelete)
-//    {
-//        await _dl.RemoveBandMember(memberToDelete);
-//    }
+   [HttpDelete]
+   public async Task Delete(BandMember memberToDelete)
+   {
+       await _dl.RemoveBandMember(memberToDelete);
+   }
 
-//}
+}
