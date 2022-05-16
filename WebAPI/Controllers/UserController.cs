dc158d84afd817f07f7ac2cc906a4a1c175ad53f
NovaFlash321 <novaflash321@gmail.com>
2022-05-16 11:07:28 -0400

    fixed route issue

--- WebAPI/Controllers/UserController.cs
+++ WebAPI/Controllers/UserController.cs
@@ -1,30 +1,30 @@
-    using Microsoft.AspNetCore.Mvc;
+//using Microsoft.AspNetCore.Mvc;
 
 
-namespace WebAPI.Controllers;
-[ApiController]
-[Route("[controller]")]
-public class UserController : ControllerBase
-{
-    private readonly DBInterface _db;
-    public UserController(DBInterface db)
-    {
-        _db = db;
-    }
-    [HttpGet]
-    public async Task<User> login(User user)
-    {
-        return await _db.loginUser(user);
-    }
-    [HttpPost]
-    public async Task<User> createUser(User user)
-    {
-        return await _db.createUser(user);
-    }
-}
-/*
-    post createuser
-    get login
-    both methods pass into the DatabaseCalls.cs methods and return the user created/logged in
-    ~leo
-*/
+//namespace WebAPI.Controllers;
+//[Route("[controller]")]
+//[ApiController]
+//public class UserController : ControllerBase
+//{
+//    private readonly DBInterface _db;
+//    public UserController(DBInterface db)
+//    {
+//        _db = db;
+//    }
+//    [HttpGet]
+//    public async Task<User> login(User user)
+//    {
+//        return await _db.loginUser(user);
+//    }
+//    [HttpPost]
+//    public async Task<User> createUser(User user)
+//    {
+//        return await _db.createUser(user);
+//    }
+//}
+///*
+//    post createuser
+//    get login
+//    both methods pass into the DatabaseCalls.cs methods and return the user created/logged in
+//    ~leo
+//*/
