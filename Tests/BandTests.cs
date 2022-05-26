21b85e9f2c66ca23549ba87bce72b1d71a1de529
Arrion7 <102093283+Arrion7@users.noreply.github.com>
2022-05-26 12:19:16 -0400

    small update

---
+++ Tests/BandTests.cs
@@ -0,0 +1,32 @@
+using Xunit;
+using Moq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
+using Models;
+using Datalayer;
+using WebAPI.Controllers;
+
+namespace Tests;
+
+public class BandTests
+{
+    public Mock<DBInterface> mock = new Mock<DBInterface>();
+
+    [Fact]
+    public async Task CreateBand_ControllerCallsDBMethod()
+    {
+        Band newBand = new Band()
+        {
+            id = 1,
+            bandId = 1,
+            //description
+            //member list
+        };
+
+        mock.Setup(p => p.CreateBand(newBand).ReturnsAsync(memberCheck);
+        BandController band = new BandController(mock.Object);
+        Band result = await band.Post(newBand);
+        Assert.Equal(band, result);
+    }
+}
