0cefda52beb3d99ef8af657f3f36d355c9a9b2b4
unknown <blchilds55@gmail.com>
2022-05-27 18:44:28 -0500

    adding showComments to the Post table

---
+++ Datalayer/Migrations/20220527234143_AddshowComments.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Datalayer.Migrations
+{
+    public partial class AddshowComments : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "showComments",
+                table: "Posts",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "showComments",
+                table: "Posts");
+        }
+    }
+}
