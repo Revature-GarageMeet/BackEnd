f28beb216fe3da8bf3631e17c4b9032fa88b1d0e
Leo Doan <doanleo@yahoo.com>
2022-05-31 17:56:08 -0400

    Opened LoginTests.cs

---
+++ Tests/LoginTest.cs
@@ -0,0 +1,18 @@
+using Xunit;
+using Moq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
+using Models;
+using Datalayer;
+using WebAPI.Controllers;
+
+namespace Tests
+{
+    public class LoginTest
+    {
+        //This class tests things related to the user and logging in
+        public Mock<DBInterface> mock = new Mock<DBInterface>();
+        
+    }
+}
